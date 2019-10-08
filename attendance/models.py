58f94d49ad4d9eb2f03bab04309f6d537e6fb480
Dennis <denniskamau3@gmail.com>
2019-10-08 12:48:45 +0300

    Add attendace Query

--- attendance/models.py
+++ attendance/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Attendance(models.Model):
+    isPresent = models.BooleanField()
+    childName = models.CharField(max_length=30)
+    
--- attendance/schema.py
+++ attendance/schema.py
@@ -1,8 +1,16 @@
 import graphene
 from graphene_django import DjangoObjectType
+from .models import Attendance
 
-class Query():
-    pass
+
+class AttendaceType(DjangoObjectType):
+    class Meta:
+        model = Attendance
+class Query(graphene.ObjectType):
+    attendance = graphene.List(AttendaceType)
+
+    def resolve_classes(self,info,**kwargs):
+        return Attendance.objects.all()
 
 class Mutation():
     pass
