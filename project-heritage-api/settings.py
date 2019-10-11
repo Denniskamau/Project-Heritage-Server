6282793498bbd0d4e2f2f828eb7b49a1b39cada3
Victor Kariuki <karizvic@gmail.com>
2019-10-11 08:39:11 +0300

    update the allowed hosts

--- project-heritage-api/settings.py
+++ project-heritage-api/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = ['project-heritage-api.herokuapp.com']
+ALLOWED_HOSTS = ['project-heritage-api.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
