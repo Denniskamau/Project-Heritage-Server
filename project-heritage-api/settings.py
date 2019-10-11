85e1aeec9626c41e80ee67212bb49788f6d61662
Victor Kariuki <karizvic@gmail.com>
2019-10-11 04:45:58 +0300

    add herokuapp url to settings.py

--- project-heritage-api/settings.py
+++ project-heritage-api/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['project-heritage-api.herokuapp.com']
 
 
 # Application definition
