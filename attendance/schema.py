e72ec50f7d8ab829492a9c64f9b6a3a1ed86d922
Dennis <denniskamau3@gmail.com>
2019-10-11 13:30:39 +0300

    finish models

--- attendance/schema.py
+++ attendance/schema.py
@@ -1,36 +1,38 @@
 import graphene
 from graphene_django import DjangoObjectType
-from .models import Attendance
+# from .models import Attendance
 
 
-class AttendaceType(DjangoObjectType):
-    class Meta:
-        model = Attendance
+# class AttendaceType(DjangoObjectType):
+#     class Meta:
+#         model = Attendance
 class Query(graphene.ObjectType):
-    attendance = graphene.List(AttendaceType)
+    # attendance = graphene.List(AttendaceType)
 
-    def resolve_classes(self,info,**kwargs):
-        return Attendance.objects.all()
+    # def resolve_classes(self,info,**kwargs):
+    #     return Attendance.objects.all()
+    pass
 
 
-class CreateAttendace(graphene.Mutation):
-    id = graphene.Int()
-    isPresent = graphene.Boolean()
-    childName = graphene.String()
+# class CreateAttendace(graphene.Mutation):
+#     id = graphene.Int()
+#     isPresent = graphene.Boolean()
+#     childName = graphene.String()
 
-    class Arguments:
-        isPresent = graphene.Boolean()
-        childName = graphene.String()
+#     class Arguments:
+#         isPresent = graphene.Boolean()
+#         childName = graphene.String()
 
-    def mutate(self,info,isPresent,childName):
-        newAttendace = Attendance(isPresent=isPresent,childName=childName)
-        newAttendace.save()
+#     def mutate(self,info,isPresent,childName):
+#         newAttendace = Attendance(isPresent=isPresent,childName=childName)
+#         newAttendace.save()
 
-        return CreateAttendace(
-            id=newAttendace.id,
-            isPresent = newAttendace.isPresent,
-            childName = newAttendace.childName
-        )
+#         return CreateAttendace(
+#             id=newAttendace.id,
+#             isPresent = newAttendace.isPresent,
+#             childName = newAttendace.childName
+#         )
 
 class Mutation(graphene.ObjectType):
-    create_attendance = CreateAttendace.Field()
+    # create_attendance = CreateAttendace.Field()
+    pass
