cd19da14d04588ea3df4d451fa4a0c5d3eb53f12
Dennis <denniskamau3@gmail.com>
2019-10-14 20:11:57 +0300

    query and muration for class working well

--- classes/schema.py
+++ classes/schema.py
@@ -9,22 +9,36 @@ class PhClassType(DjangoObjectType):
 class CreateClass(graphene.Mutation):
     id = graphene.Int()
     ageGroup = graphene.String()
-    facilitator = graphene.String()
+    class_designation = graphene.String()
+    name = graphene.String()
+    min_age = graphene.Int()
+    max_age = graphene.Int()
 
     """Define the data to be sent ot the server"""
     class Arguments:
         ageGroup = graphene.String()
-        facilitator = graphene.String()
+        class_designation = graphene.String()
+        name = graphene.String()
+        min_age = graphene.Int()
+        max_age = graphene.Int()
+
 
     """Save the data sent by the user to the db"""
-    def mutate(self,info,ageGroup,facilitator):
-        newClass =  PhClass(ageGroup=ageGroup,facilitator=facilitator)
+    def mutate(self,info,**kwargs):
+        newClass =  PhClass(ageGroup=kwargs['ageGroup'],
+                    class_designation = kwargs['class_designation'],
+                    name= kwargs['name'],
+                    max_age=kwargs['max_age'],
+                    min_age=kwargs['min_age'])
         newClass.save()
         """Return the newly created data"""
         return CreateClass(
             id = newClass.id,
             ageGroup = newClass.ageGroup,
-            facilitator = newClass.facilitator
+            class_designation = newClass.class_designation,
+            name = newClass.name,
+            max_age = newClass.max_age,
+            min_age = newClass.min_age
         )
 class Query(graphene.ObjectType):
     classes = graphene.List(PhClassType)
