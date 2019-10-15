1519bd51796148edcfcb3b92c059195abce36f8d
Dennis <denniskamau3@gmail.com>
2019-10-15 19:42:06 +0300

    finish update for classes

--- classes/schema.py
+++ classes/schema.py
@@ -2,44 +2,15 @@ import graphene
 from graphene_django import DjangoObjectType
 from .models import PhClass
 
+
+from .mutations import CreateClass,UpdateClass
+
+
 class PhClassType(DjangoObjectType):
     class Meta:
         model = PhClass
 
-class CreateClass(graphene.Mutation):
-    id = graphene.Int()
-    ageGroup = graphene.String()
-    class_designation = graphene.String()
-    name = graphene.String()
-    min_age = graphene.Int()
-    max_age = graphene.Int()
 
-    """Define the data to be sent ot the server"""
-    class Arguments:
-        ageGroup = graphene.String()
-        class_designation = graphene.String()
-        name = graphene.String()
-        min_age = graphene.Int()
-        max_age = graphene.Int()
-
-
-    """Save the data sent by the user to the db"""
-    def mutate(self,info,**kwargs):
-        newClass =  PhClass(ageGroup=kwargs['ageGroup'],
-                    class_designation = kwargs['class_designation'],
-                    name= kwargs['name'],
-                    max_age=kwargs['max_age'],
-                    min_age=kwargs['min_age'])
-        newClass.save()
-        """Return the newly created data"""
-        return CreateClass(
-            id = newClass.id,
-            ageGroup = newClass.ageGroup,
-            class_designation = newClass.class_designation,
-            name = newClass.name,
-            max_age = newClass.max_age,
-            min_age = newClass.min_age
-        )
 class Query(graphene.ObjectType):
     classes = graphene.List(PhClassType)
 
@@ -48,6 +19,8 @@ class Query(graphene.ObjectType):
 
 
 
+
 """Create a mutation class for the fields to be resolved"""
 class Mutation(graphene.ObjectType):
     create_class = CreateClass.Field()
+    update_class = UpdateClass.Field()
